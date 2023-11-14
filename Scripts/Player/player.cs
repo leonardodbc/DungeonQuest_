9f53f37e94776e5ceb9e8296813bb2c33570dcd1
Leonardo de Borba Cardoso <126882967+leonardodbc@users.noreply.github.com>
2023-11-14 09:51:01 -0300

    Let There Be Light

---
+++ Scripts/Player/player.cs
@@ -0,0 +1,55 @@
+using Godot;
+using System;
+
+public partial class player : CharacterBody3D
+{
+	public const float Speed = 5.0f;
+	public const float JumpVelocity = 4.5f;
+
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+
+	private AnimationTree animations;
+	private Node3D visual;
+	public override void _Ready(){
+
+		animations = GetNode<AnimationTree>("AnimationTree");
+
+        visual = GetNode<Node3D>("rig");
+
+    }
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+			velocity.Y -= gravity * (float)delta;
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+			velocity.Y = JumpVelocity;
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 inputDir = Input.GetVector("left", "right", "forward", "backward");
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+
+			direction = direction.Normalized();
+        	GetNode<Node3D>("Rig").LookAt(Position + direction, Vector3.Up);
+		}
+		else
+		{
+			
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
