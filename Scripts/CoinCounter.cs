368f429c7d7d41bbd8277aefc807b34529b60fe1
Leonardo de Borba Cardoso <126882967+leonardodbc@users.noreply.github.com>
2023-11-16 14:12:48 -0300

    add hud and coin

--- Scripts/CoinCounter.cs
+++ Scripts/CoinCounter.cs
@@ -1,23 +1,18 @@
 using Godot;
 using System;
 
-public partial class SpikeArea : Area3D
+public partial class CoinCounter : Label
 {
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Text = "0";
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		
 	}
-	
-	private void _on_body_entered(Node3D body)
-	{
-		if (body is player) {
-			body.GlobalPosition = new Vector3(0, 0, 0);
-		}
-	}
-
 }
